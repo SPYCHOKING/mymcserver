0433ed83efa8390316dd499d05f356390b5bcf3a
SPYCHOKING <102844770+SPYCHOKING@users.noreply.github.com>
2025-11-20 12:21:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Download server.jar if missing
 if [ ! -f server.jar ]; then
-    wget -O server.jar https://piston-data.mojang.com/v1/objects/69b4aeff4d461a775888b859504a829148d464ef/server.jar
+    wget -O server.jar https://piston-data.mojang.com/v1/objects/1ad21c011df9290ecb7bd54e39dbf99ed5b6848f/server.jar
 fi
 
-# Accept EULA
 echo "eula=true" > eula.txt
 
-# Start Minecraft
 java -Xmx1024M -Xms1024M -jar server.jar nogui
