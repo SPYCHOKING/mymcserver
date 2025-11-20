bf302fe34ac8eb2d09610a6c0b1731b03e54be9b
SPYCHOKING <102844770+SPYCHOKING@users.noreply.github.com>
2025-11-20 12:32:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Download latest Minecraft server.jar (1.21.4)
 if [ ! -f server.jar ]; then
-    wget -O server.jar https://launcher.mojang.com/v1/objects/45810d238246d90e811d896f87b14695b7fb6839/server.jar
+    echo "Downloading server.jar..."
+    wget -O server.jar https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
 fi
 
-# Accept EULA
 echo "eula=true" > eula.txt
 
-# Start server
 java -Xmx1024M -Xms1024M -jar server.jar nogui
