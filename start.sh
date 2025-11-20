bd0a51403d0f3f232c14f4d801a3e77ad6e2e37d
SPYCHOKING <102844770+SPYCHOKING@users.noreply.github.com>
2025-11-20 11:53:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# Make sure we’re in the directory of this script (optional)
+cd "$(dirname "$0")"
 
 # Download server.jar if it doesn't exist
 if [ ! -f server.jar ]; then
-    wget -O server.jar https://launcher.mojang.com/v1/objects/45810d238246d90e811d896f87b14695b7fb6839/server.jar
+    wget -O server.jar https://launcher.mojang.com/v1/objects/4f81d0328246d90e811f9cda525c20bbd76ecf43/server.jar
 fi
 
-# Accept EULA
-echo "eula=true" > eula.txt
+# Accept the EULA
+echo "# By changing the setting below to TRUE you are indicating your agreement to our EULA (https://aka.ms/MinecraftEULA)" > eula.txt
+echo "eula=true" >> eula.txt
 
-# Start Minecraft server
+# Start the server with memory limits
 java -Xmx1024M -Xms1024M -jar server.jar nogui
-
-chmod +x start.sh
